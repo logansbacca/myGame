97184483bfefc47b58a824851702afc95cd316ed
logansbacca <61002844+logansbacca@users.noreply.github.com>
2020-09-30 17:11:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,37 +6,114 @@ class Player {
         this.x = x;
         this.y = y;
         this.w = 40;
-        this.h = 80;
+        this.h = 80;       
+        this.ySpeed = 5;
+        this.xSpeed = 0;
     }
     show() {
         var img = new Image();
         img.src = 'panda2.png';
         img.width = 2;
         img.height= 4;
-    
-        img.onload = () => {
-            ctx.drawImage(img,this.x,this.y,100,100);
-            ctx.moveTo(300,300); 
-        }
+        ctx.drawImage(img,this.x,this.y,100,100);
         
     }
+    update() {
+        
+         this.y += this.ySpeed;
+         this.ySpeed += gravity;
+        
+        if (this.y >= 750-80) {
+            this.ySpeed = 0;
+            canJump = true;
+        } else {
+            canJump = false;
+        }
+    }
+}
 
+class Rock {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.w = 80;
+        this.h = 80;
+    }
+    show() {
+
+        var img = new Image();
+        img.src = 'bamboo.png';
+        img.width = 2;
+        img.height= 4;
+        ctx.drawImage(img,this.x,this.y,120,120);
+       
+    }
+    update() {
+         if (this.x < p.x + p.w && this.x + this.w > p.x && this.y < p.y + p.h && this.y + this.h > p.y) {
+        location.reload();
+        }
+    }
 }
 
 var p;
+var gravity = 0.1
 
-window.onload = function() {
+var canJump = true;
+
+var rocks = [];
+
+var rockX = 800;
+
+var score = 0;
+
+function gmz () {
     start();
-    update();
+    setInterval(update,100);
+    setInterval(changeSpeed, 500);
+    setInterval(increaseScore, 500);
 }
+
 function start() {
-    p = new Player (100,650)
+    p = new Player(100, 400);
+    
+    
+    for (let i = 0; i < 100; i++) {
+        var r = new Rock(rockX, 650);
+        rocks.push(r);
+        rockX += Math.floor(Math.random() * 500) + 300;
+    }
+    
+    p.xSpeed = 5;
 }
 
 function update() {
-    canvas.width = canvas.width;
-    ctx.fillStyle = "#DEB887";
-    ctx.fillRect(0, 750, 800, 800);
+    canvas.width=canvas.width;
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 750, 800, 100);
     p.show();
+    p.update();
+    for (let i = 0; i < rocks.length; i++) {
+        rocks[i].show();
+        rocks[i].update();
+        rocks[i].x -= p.xSpeed;
+    }
+
+    document.getElementById("showScore").innerHTML = "Score: " + score;
 }
+
+function changeSpeed() {
+    p.xSpeed += 0.1;
+}
+
+function increaseScore() {
+    score++;
+}
+
+function keyDown(e) {
+    if (e.keyCode === 32 && canJump) {
+        p.ySpeed = -10;
+    }
+}
+
+document.onkeypress = keyDown;
 
